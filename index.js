ca3d34607da0430b09e26a315e4df81b90573e5f
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-07-24 11:54:57 -0400

    httpdispatcher update

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+// require the http module of node.js
+var http = require('http');
+// require the dispatcher module
+var httpdispatcher = require('httpdispatcher');
+var dispatcher = new httpdispatcher();
+// define the port of access for your server
+const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello Node!\n');
+// We need a function which handles requests and send response
+function handleRequest(request, response){
+    try {
+        // log the request on console
+        console.log(request.url);
+        // Dispatch
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+// Create a server
+var myFirstServer = http.createServer(handleRequest);
+
+// add some routes
+
+//A sample GET request
+dispatcher.onGet("/", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hey, this is the homepage of your server</h1>');
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+dispatcher.onGet("/welcome", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Welcome homepage');
+});
+
+dispatcher.onError(function(req, res) {
+    res.writeHead(404);
+    res.end("Error, the URL doesn't exist");
+});
+
+// Start the server !
+myFirstServer.listen(PORT, function(){
+    // Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
